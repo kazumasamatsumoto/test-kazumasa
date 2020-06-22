432aef0e7514b7a2449154c97d034158fc054d1e
松本一将 <kazumasa.matsumoto62@gmail.com>
2020-06-22 19:28:44 +0900

    作成

---
+++ app/Http/Controllers/BookmarkController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bookmark;
+use Illuminate\Http\Request;
+
+class BookmarkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $bookmarks = Bookmark::orderBy('id', 'desc')->paginate(20);
+
+        return view('bookmarks.index', ['bookmarks' => $bookmarks]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('bookmarks.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Bookmark::create($request->all());
+
+        return redirect()->route('bookmarks.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Bookmark  $bookmark
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Bookmark $bookmark)
+    {
+        return view('bookmarks.show', ['bookmark' => $bookmark]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Bookmark  $bookmark
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Bookmark $bookmark)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Bookmark  $bookmark
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bookmark $bookmark)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Bookmark  $bookmark
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Bookmark $bookmark)
+    {
+        //
+    }
+}
---
+++ resources/views/bookmarks/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">ブックマーク一覧</div>
+
+                <div class="card-body">
+                    <div class="mb-3">
+                        <a href="{{ route('bookmarks.create') }}" class="btn btn-primary">新規登録</a>
+                    </div>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>タイトル </th>
+                            </tr>
+                        </thead>
+                        @foreach ($bookmarks as $bookmark)
+                            <tr>
+                                <td>{{ $bookmark->id }}</td>
+                                <td><a href="{{ route('bookmarks.show', $bookmark->id) }}">{{ $bookmark->title }}</a></td>
+                            </tr>
+                        @endforeach
+                    </table>
+                    {{ $bookmarks->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
